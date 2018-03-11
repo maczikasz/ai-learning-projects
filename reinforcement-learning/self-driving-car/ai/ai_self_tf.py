6e0f844c8c58d8b32c5e9f757bb78ee0f1eacc28
Szava <Szava.Maczika@swisscom.com>
2018-03-11 23:24:52 +0100

    Remove commented lines from TF add back hidden layer, fix loading procedure

--- reinforcement-learning/self-driving-car/ai/ai_self_tf.py
+++ reinforcement-learning/self-driving-car/ai/ai_self_tf.py
@@ -42,8 +42,8 @@ class Dqn():
         self.input_tensor = tf.placeholder(shape=[None, input_size], dtype=tf.float32)
 
         self.fc1 = slim.fully_connected(inputs=self.input_tensor, num_outputs=30, activation_fn=tf.nn.relu, scope="fc1")
-        # self.fc2 = slim.fully_connected(inputs=self.fc1, num_outputs=30, activation_fn=tf.nn.relu, scope="fc2")
-        self.q = slim.fully_connected(inputs=self.fc1, num_outputs=nb_action, activation_fn=None, scope="q")
+        self.fc2 = slim.fully_connected(inputs=self.fc1, num_outputs=30, activation_fn=tf.nn.relu, scope="fc2")
+        self.q = slim.fully_connected(inputs=self.fc2, num_outputs=nb_action, activation_fn=None, scope="q")
         self.softmax = slim.softmax(self.q * 70, scope="softmax")
         slim.summary.tensor_summary("softmax", self.softmax)
         self.chosen_action = tf.argmax(self.softmax, axis=1)
@@ -54,24 +54,14 @@ class Dqn():
         self.hot = slim.one_hot_encoding(self.action, self.num_action, scope="one_hot")
         self.predictions = tf.reduce_sum(self.hot * self.q, axis=1)
         self.loss = tf.reduce_sum((self.predictions - self.target) ** 2)
-        # self.loss = slim.losses.mean_squared_error(predictions=self.predictions, labels=self.target)
 
         self.optimizer = slim.train.AdamOptimizer()
-        # slim.losses.get_total_loss()
-        # slim.summary.scalar("loss", self.loss)
-        # slim.summary.histogram("loss", self.loss)
 
         self.training = slim.learning.create_train_op(total_loss=self.loss, optimizer=self.optimizer,
                                                       summarize_gradients=True)
 
-        # self.grads_and_vars = self.optimizer.compute_gradients(self.loss)
-        # Change grads_and_vars as you wish
-        # self.training = self.optimizer.apply_gradients(self.grads_and_vars)
-
         sess = tf.Session()
         self.sess = sess
-        # self.sess = tf_debug.LocalCLIDebugWrapperSession(sess)
-        # self.sess = tf_debug.TensorBoardDebugWrapperSession(sess, "localhost:54027")
 
         self.ctr = 0
 
@@ -106,11 +96,6 @@ class Dqn():
 
         q_orig, softmax, action_value = self.sess.run([self.q, self.softmax, self.chosen_action],
                                                       feed_dict={self.input_tensor: [new_signal]})
-        # print "Qvalues"
-        # print q_orig
-        # print "Qvalues after softmax"
-        # print softmax
-        # print reward
         action = action_value[0]
 
         if len(self.memory.memory) > 300:
@@ -129,9 +114,13 @@ class Dqn():
     def save(self, filename):
         self.saver.save(self.sess, filename)
 
-    def load(self, filename):
-        if os.path.exists(filename):
+    def load(self, filepath):
+        if os.path.exists(filepath):
             print("===>> Loading checkpoint ...")
-            self.saver.restore(self.sess, filename)
+            filepath, _ = os.path.splitext(filepath)
+            self.saver = tf.train.import_meta_graph(filepath + ".meta")
+            # dir, filename = os.path.split(filepath)
+            self.saver.restore(self.sess, filepath)
+            print("Loaded checkpoint")
         else:
             print("Nothing to load")
